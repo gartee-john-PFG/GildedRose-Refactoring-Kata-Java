dcc2b1f6ae80a4f562f70f0818dcefb2cb5c1f01
Steve Turley <steve@turleylabs.com>
2020-05-11 15:12:34 -0500

    Code Smell: Switch Statements. Refactor: Replace conditional with polymorphism

--- src/main/java/com/gildedrose/Sulfuras.java
+++ src/main/java/com/gildedrose/Sulfuras.java
@@ -4,4 +4,50 @@ public class Sulfuras extends Item {
     public Sulfuras(int sellIn, int quality) {
         super("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
+
+    @Override
+    void updateItem() {
+        if (!name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sellIn < 11) {
+                        increaseQualityWhenBelow50();
+                    }
+
+                    if (sellIn < 6) {
+                        increaseQualityWhenBelow50();
+                    }
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                increaseQualityWhenBelow50();
+            }
+        }
+    }
 }
