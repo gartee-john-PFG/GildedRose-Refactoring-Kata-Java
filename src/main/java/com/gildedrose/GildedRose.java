19f8fa006ec7e1abfbf4a6f35a9389d22a473e78
Steve Turley <steve@turleylabs.com>
2020-05-11 14:13:20 -0500

    Code Smell: Feature Envy. Refactor: Move method.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,52 +9,7 @@ class GildedRose {
 
     public void updateItems() {
         for (Item item : items) {
-            updateItem(item);
-        }
-    }
-
-    private void updateItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        item.increaseQualityWhenBelow50();
-                    }
-
-                    if (item.sellIn < 6) {
-                        item.increaseQualityWhenBelow50();
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                item.increaseQualityWhenBelow50();
-            }
+            item.updateItem();
         }
     }
 
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -28,4 +28,49 @@ public class Item {
             quality = quality + 1;
         }
     }
+
+    void updateItem() {
+        if (!name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sellIn < 11) {
+                        increaseQualityWhenBelow50();
+                    }
+
+                    if (sellIn < 6) {
+                        increaseQualityWhenBelow50();
+                    }
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                increaseQualityWhenBelow50();
+            }
+        }
+    }
 }
