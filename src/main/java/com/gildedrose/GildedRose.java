6a36f55dd86ca69b7cf403b716f30a336b226d62
Steve Turley <steve@turleylabs.com>
2020-05-11 15:09:56 -0500

    Code Smell: Switch Statements. Refactor: Introduce Subclasses

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,25 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     Item[] items;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        this.items = Arrays.stream(items)
+                .map(this::createItem)
+                .toArray(Item[]::new);
+    }
+
+    private Item createItem(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            return new AgedBrie(item.sellIn, item.quality);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new BackstagePasses(item.sellIn, item.quality);
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new Sulfuras(item.sellIn, item.quality);
+        }
+        return item;
     }
 
     public void updateItems() {
